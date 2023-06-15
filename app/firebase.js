5677daafefe4180af4b054b74def1a9b5b1b996c
Pablo GP <garciaplaza.pablo@gmail.com>
2023-06-15 14:08:19 +0200

    Uncommented

--- app/firebase.js
+++ app/firebase.js
@@ -1,11 +1,11 @@
-// import {
-//   API_KEY,
-//   AUTH_DOMAIN,
-//   PROJECT_ID,
-//   STORAGE_BUCKET,
-//   MESSAGING_SENDER_ID,
-//   APP_ID,
-// } from "./settings.js";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "./settings.js";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
